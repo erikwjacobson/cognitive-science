2459c3c6d1958b5b47642f7506c91b9a57706b0b
Erik Jacobson <erik@jacobsonfarm.com>
2020-03-02 16:28:22 -0600

    Fixed the issue with incorrect storage of course groups

--- resources/views/degrees/courses/edit.blade.php
+++ resources/views/degrees/courses/edit.blade.php
@@ -63,11 +63,11 @@
                             </div>
                             <div class="col-md-2">
                                 <label class="required">Minimum # of Courses</label>
-                                {!! Form::select('subgroup', ['NA' => 'NA',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40], null, ['class' => 'form-control select2-single', 'id' => 'requirement-score-sub']) !!}
+                                {!! Form::select('subgroup', ['NA',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40], null, ['class' => 'form-control select2-single', 'id' => 'requirement-score-sub']) !!}
                             </div>
                             <div class="col-md-2">
                                 <label class="required"># Of Alternatives</label>
-                                {!! Form::select('group', ['NA' => 'NA',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40], null, ['class' => 'form-control  select2-single', 'id' => 'requirement-score-group']) !!}
+                                {!! Form::select('group', ['NA',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40], null, ['class' => 'form-control  select2-single', 'id' => 'requirement-score-group']) !!}
                             </div>
                         </div>
                         <br>
